e1a1e510a0ae70b9c2afef5c5e73e17992f9eef1
abhijithmahipal <abhijithmahipal@gmail.com>
2022-06-18 16:42:24 +0530

    feat: added sidebar initial

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  public sidebarOpened = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  toggleSidebar() {
+    this.sidebarOpened = !this.sidebarOpened;
   }
 
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        theme:'#00091a'
+      }
+    }
    },
   plugins: []
 }
