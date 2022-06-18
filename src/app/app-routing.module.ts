59338bfb1fd1bf749bcf452001bfaf6075384913
abhijithmahipal <abhijithmahipal@gmail.com>
2022-06-18 13:54:11 +0530

    feat: added header and component routing initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { SkillComponent } from './skill/skill.component';
 
-const routes: Routes = [];
+  const routes: Routes = [
+    {path: '', component: HomeComponent },
+    {path: 'skill', component: SkillComponent },
+    {path: 'portfolio', component: PortfolioComponent },
+    {path: 'aboutme', component: AboutMeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { HomeComponent } from './home/home.component';
+import { SkillComponent } from './skill/skill.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SidebarComponent } from './core/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SkillComponent,
+    PortfolioComponent,
+    AboutMeComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
